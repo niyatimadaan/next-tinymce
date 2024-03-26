76de1e5286098fcf6262a6aa459df91b6d9fb7a1
niyatimadaan <madaanniyati@gmail.com>
2024-03-26 17:46:34 +0530

    bug fixes for prod

--- src/app/components/uploader.tsx
+++ src/app/components/uploader.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { UploadButton } from "@/utils/uploadthing";
+import { UploadButton } from "@uploadthing/react";
+import { OurFileRouter } from "../api/uploadthing/core";
+
+// import { UploadButton } from "@/utils/uploadthing";
  
 export default function UploaderButton() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UploadButton
+      <UploadButton<OurFileRouter, any>
         endpoint="textUploader"
-        onClientUploadComplete={(res) => {
+        onClientUploadComplete={(res:any) => {
           // Do something with the response
           console.log("Files: ", res);
           alert("Upload Completed");
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-between">
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <div className="text-4xl font-bold mb-4" >Welcome to our home page!</div>
-        <div className="text-xl mb-8" >Let's get started with our app.</div>
+        <div className="text-xl mb-8" >Let&apos;s get started with our app.</div>
         <div className="flex items-center">
           <Link href="/register" className="mb-4 mr-4">
             Register
